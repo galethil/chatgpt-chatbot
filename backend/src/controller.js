29f98c8c37b0aeeca487d22dfb4cfd9c527b6801
Peter Baus <peter.baus@accenture.com>
2024-03-04 18:04:14 +0100

    Prepare session 4

--- backend/src/controller.js
+++ backend/src/controller.js
@@ -1,30 +1,9 @@
-const { request, moderation } = require('./chatGptApi');
-const { delimiter, moderationFailMessage, delimiterBackTick } = require('./constants');
-const { inputModerationPrompt } = require('./prompts');
-const { ENABLE_INPUT_MODERATION } = require('../config');
+const { request } = require('./chatGptApi');
 
 const prompt = async (message) => {
-  // moderation replacement of delimiters
-  const cleanedUserMessage = message.replace(delimiter, '').replace(delimiterBackTick, '');
-
-  // moderation check of message
-  if (cleanedUserMessage.length > 1000) {
-    return 'Your request is too long. Try shorter request or contact out support.';
-  }
-  if (ENABLE_INPUT_MODERATION) {
-    const moderationMessages = [
-      { role: 'system', content: inputModerationPrompt },
-      { role: 'user', content: `${delimiter}${cleanedUserMessage}${delimiter}` }
-    ];
-    const moderationOutput = await request(moderationMessages);
-    if (moderationOutput === 'Y') {
-      return moderationFailMessage;
-    }
-  }
-
   const messages = [
     { role: 'system', content: '' },
-    { role: 'user', content: `${delimiter}${cleanedUserMessage}${delimiter}` }
+    { role: 'user', content: message }
   ];
   const output = await request(messages);
 
