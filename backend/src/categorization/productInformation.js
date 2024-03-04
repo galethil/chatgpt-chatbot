a3ceab6f354ee4de86138a508b5e28bcd017f2f8
Peter Baus <peter.baus@accenture.com>
2024-03-04 13:02:46 +0100

    MOCK info in product information

--- backend/src/categorization/productInformation.js
+++ backend/src/categorization/productInformation.js
@@ -13,7 +13,13 @@ const productInformation = async (cleanedUserMessage) => {
   productInfoCategorizationOutput = await request(productInfoCategorizationMessages);
 
   if (productInfoCategorizationOutput === '1') {
+    return 'Query DB for the product specific info.';
   } else if (productInfoCategorizationOutput === '2') {
+    return 'Query DB for the cheapest product.';
+  } else if (productInfoCategorizationOutput === '3') {
+    return 'Query DB for the most expensive product.';
+  } else if (productInfoCategorizationOutput === '4') {
+    return 'Prepared response about the phone camera';
   }
   return 'Product information from DB';
 };
