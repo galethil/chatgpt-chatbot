0c1073961cb8ca3b3487e3f3bd72f2a5093e3d68
Peter Baus <peter.baus@accenture.com>
2024-03-11 09:08:15 +0100

    README + NODE_ENV removal

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const config = Object.freeze({
-  NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT || 4500,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   DEBUG: process.env.DEBUG === 'true' ? true : false,
